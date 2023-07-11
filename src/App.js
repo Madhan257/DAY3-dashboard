82a40cac4f054b283c39366b87a7500eed66bce1
Madhan257 <113665242+Madhan257@users.noreply.github.com>
2023-07-11 16:04:12 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Dashboard from './dashboard';
+function App() {
+  return (
+    <div>
+      <Dashboard/>
+    </div>
+  );
+}
+export default App;
---
+++ src/dashboard.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import './dash.css';
+function dashboard() {
+  return (
+    <div>
+        <header>
+    
+            
+                <div class="logo">Recurito</div>
+      
+           
+        </header>
+        <div class="main-container">
+            <div class="navcontainer">
+                <nav class="nav">
+                    <div class="nav-upper-options">
+                       
+
+                        <div class="nav-option option5">
+                           
+                            <h3> Profile</h3>
+                        </div>
+    
+    
+                        <div class="option2 nav-option">
+                           
+                            <h3> Find Jobs</h3>
+                        </div>
+                        <div class="nav-option option4">
+                           
+                            <h3> Companies</h3>
+                        </div>
+    
+                        <div class="nav-option option5">
+                           
+                            <h3> Cources</h3>
+                        </div>
+    
+                        <div class="nav-option option6">
+                            
+                            <h3> Settings</h3>
+                        </div>
+    
+                        <div class="nav-option logout">
+                            
+                            <h3>Logout</h3>
+                        </div>
+    
+                    </div>
+                </nav>
+            </div>
+            <div class="main">
+    
+               
+                </div>
+              
+             </div>
+        </div>
+ 
+           
+
+)
+}
+
+export default dashboard
